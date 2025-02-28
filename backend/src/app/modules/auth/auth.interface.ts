43fe97c61903b04a8ee02b06b47fe8ff50889280
naymHdev <naym100m@gmail.com>
2025-03-01 00:13:53 +0600

    [created] auth modules

---
+++ backend/src/app/modules/auth/auth.interface.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+
+import { Document, Model } from 'mongoose';
+
+// Enum for User Roles
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+// Interface for client information
+interface IUserInfo {
+  device: 'pc' | 'mobile'; // Device type
+  browser: string; // Browser name
+  ipAddress: string; // User IP address
+  pcName?: string; // Optional PC name
+  os?: string; // Optional OS name (Windows, MacOS, etc.)
+  userAgent?: string; // Optional user agent string
+}
+
+// User Schema Definition
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  role: UserRole;
+  userInfo: IUserInfo;
+  lastLogin: Date;
+  isActive: boolean;
+  otpToken: string | null; // Explicitly set as nullable
+  readonly createdAt: Date; // `readonly` to prevent manual modification
+  updatedAt: Date;
+}
+
+export interface UserModel extends Model<IUser> {
+  //instance methods for checking if passwords are matched
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+  isUserExistsByEmail(id: string): Promise<IUser>;
+  checkUserExist(userId: string): Promise<IUser>;
+}
---
+++ backend/src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/register');
+
+router.post('/login');
+
+router.post('/logout');
+
+export const AuthRoutes = router;
