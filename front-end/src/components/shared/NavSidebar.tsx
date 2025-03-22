fd3e6341468cb22610f2d321ac9c1790c077dbec
naymHdev <naym100m@gmail.com>
2025-03-22 13:05:32 +0600

    [feat] implement category fetching in NavSidebar and update layout

--- front-end/src/components/shared/NavSidebar.tsx
+++ front-end/src/components/shared/NavSidebar.tsx
@@ -1,8 +1,12 @@
+import { getAllCategories } from "@/services/category";
+import { TCategory } from "@/types/listings";
 import { AlignJustify, CircleUserRound, FolderKanban, UserRound, X, Heart } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const NavSidebar = () => {
+    const [categories, setCategories] = useState<TCategory[] | null>([])
     const sidebarRef = useRef<HTMLDivElement>(null);
 
     const toggleSidebar = () => {
@@ -10,6 +14,20 @@ const NavSidebar = () => {
             sidebarRef.current.classList.toggle("-translate-x-full");
         }
     };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const categoryData = await getAllCategories();
+                setCategories(categoryData?.data);
+            } catch (error: any) {
+                return Error(error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    console.log('categories', categories);
 
     return (
         <>
@@ -25,7 +43,7 @@ const NavSidebar = () => {
             {/* Sidebar */}
             <div
                 ref={sidebarRef}
-                className="bg-[#F3F3F3] fixed top-0 left-0 h-full w-64 transform -translate-x-full transition-transform duration-300 z-50 shadow text-gray-700"
+                className="bg-white fixed top-0 left-0  w-64 transform -translate-x-full transition-transform duration-300 z-50 shadow text-gray-700"
             >
                 <div className="flex justify-between border-b border-neutral-300 shadow px-4 py-2">
                     <button
@@ -36,37 +54,58 @@ const NavSidebar = () => {
                     </button>
                     <X />
                 </div>
-                <div className="p-3 border-b border-neutral-300">
-                    <div className="flex items-center gap-2">
-                        <CircleUserRound />
-                        <Link href="/login" className="text-sm">
-                            Login/Register
-                        </Link>
+                <div className="h-screen overflow-auto scroll-smooth">
+                    <div className="p-3 border-b border-neutral-300">
+                        <div className="flex items-center gap-2">
+                            <CircleUserRound />
+                            <Link href="/login" className="text-sm">
+                                Login/Register
+                            </Link>
+                        </div>
                     </div>
+                    <div className="p-4 border-b border-neutral-300 space-y-4 font-medium text-sm">
+                        <div className="flex items-center gap-2">
+                            <UserRound />
+                            <Link href="/user/my-account">
+                                My Accounts
+                            </Link>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <Heart />
+                            <Link href="/user/my-favourits">
+                                Wishlist
+                            </Link>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <FolderKanban />
+                            <Link href="/user/dashboard">
+                                My Listings
+                            </Link>
+                        </div>
+                    </div>
+                    <section className="p-4">
+                        <h3 className=" font-semibold text-lg uppercase">Categories</h3>
+                        <div className=" space-y-4 mt-4">
+                            {
+                                categories && categories?.map((category: TCategory, idx: number) => (
+                                    <div key={idx} className="flex items-center gap-2 text-sm font-normal hover:text-[#1575B9]">
+                                        <div className="w-8 h-8 relative rounded-full overflow-hidden">
+                                            <Image
+                                                src={category.icon}
+                                                alt={category.name}
+                                                fill
+                                                className="object-contain"
+                                            />
+                                        </div>
+                                        <Link href={`/listings/${category._id}`}>
+                                            {category.name}
+                                        </Link>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </section>
                 </div>
-                <div className="p-4 border-b border-neutral-300 space-y-4 font-medium text-sm">
-                    <div className="flex items-center gap-2">
-                        <UserRound />
-                        <Link href="/user/my-account">
-                            My Accounts
-                        </Link>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Heart />
-                        <Link href="/user/dashboard">
-                            Wishlist
-                        </Link>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <FolderKanban />
-                        <Link href="/user/dashboard">
-                            My Listings
-                        </Link>
-                    </div>
-                </div>
-                <section className=" p-4">
-                    <h3 className=" font-semibold text-lg uppercase">Categories</h3>
-                </section>
             </div>
         </>
     );
