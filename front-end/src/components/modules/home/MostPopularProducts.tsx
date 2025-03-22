1ee0eef8076f786035b9554d028067570869281f
naymHdev <naym100m@gmail.com>
2025-03-22 20:34:38 +0600

    feat: enhance MostPopularProducts component; improve layout and add currency formatting

--- front-end/src/components/modules/home/MostPopularProducts.tsx
+++ front-end/src/components/modules/home/MostPopularProducts.tsx
@@ -10,6 +10,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Image from "next/image";
+import { currencyFormatter } from "@/utils/currencyFormatter";
 
 const MostPopularProducts = ({ data }: { data: TLIsting[] }) => {
   // console.log('object', data);
@@ -19,9 +20,16 @@ const MostPopularProducts = ({ data }: { data: TLIsting[] }) => {
       <div className="mt-4 md:mt-10">
         <PBContainer maxWidth="7xl">
           <div>
-            <h2 className=" text-lg font-semibold">Most Popular Ads</h2>
+            <div className=" flex items-center justify-between">
+              <h2 className=" text-lg font-semibold">Most Popular Ads</h2>
+              <div></div>
+            </div>
             <div className="mt-5">
-              <Carousel className=" w-10/12 max-w-6xl mx-auto">
+              <Carousel className="relative w-full max-w-7xl mx-auto">
+                <div className="absolute -top-7 right-12 -translate-x-1/2 flex gap-1">
+                  <CarouselPrevious className="border-neutral-400" />
+                  <CarouselNext className="border-neutral-400" />
+                </div>
                 <CarouselContent>
                   {data
                     ?.slice()
@@ -29,35 +37,35 @@ const MostPopularProducts = ({ data }: { data: TLIsting[] }) => {
                     ?.map((product) => (
                       <CarouselItem
                         key={product._id}
-                        className="pl-1 basis-1/1 md:basis-1/4"
+                        className="pl-1 basis-1/1 md:basis-1/3 lg:basis-1/5"
                       >
                         <div className="p-2">
-                          <Card className="bg-white border-neutral-200 rounded-2xl shadow-none">
-                            <CardContent className="flex flex-col aspect-square items-center justify-evenly ">
-                              <div className="relative w-full h-[8rem]">
-                                <Image
-                                  src={product.images[0]}
-                                  alt={product.title}
-                                  fill
-                                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                  className="object-contain rounded-md"
-                                />
-                              </div>
-                              <Link href={`/listings/${product._id}`}>
+                          <Link href={`/listings/${product._id}`}>
+                            <Card className="bg-white group border-neutral-200 rounded-2xl shadow-none">
+                              <CardContent className="flex flex-col aspect-square items-center justify-evenly ">
+                                <div className="relative w-full h-[8rem]">
+                                  <Image
+                                    src={product.images[0]}
+                                    alt={product.title}
+                                    fill
+                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                    className="object-contain rounded-md transition-transform duration-500 ease-in-out group-hover:scale-110"
+                                  />
+                                </div>
+
                                 <h2 className="mt-2 text-md font-medium text-center text-gray-800">
                                   {product.title.length > 30
                                     ? product.title.slice(0, 30) + "..."
                                     : product.title}
                                 </h2>
-                              </Link>
-                            </CardContent>
-                          </Card>
+                                <p className=" group-hover:text-[#1575B9] group-hover:font-bold">{currencyFormatter(product.price)}</p>
+                              </CardContent>
+                            </Card>
+                          </Link>
                         </div>
                       </CarouselItem>
                     ))}
                 </CarouselContent>
-                <CarouselPrevious className=" border-neutral-400" />
-                <CarouselNext className=" border-neutral-400" />
               </Carousel>
             </div>
           </div>
