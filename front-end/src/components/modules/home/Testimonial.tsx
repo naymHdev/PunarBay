d9d857ef6ab206ac7d4f87589392aca6e32fccb3
naymHdev <naym100m@gmail.com>
2025-03-23 11:09:12 +0600

    feat: enhance Testimonial section layout and functionality; add user ratings and location display

--- front-end/src/components/modules/home/Testimonial.tsx
+++ front-end/src/components/modules/home/Testimonial.tsx
@@ -8,10 +8,10 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
 import { testimonials } from "@/data/testimonials";
+import { Star } from "lucide-react";
+import PBButton from "@/components/ui/PBButton";
 
 const TestimonialSection = () => {
   const plugin = React.useRef(
@@ -20,33 +20,62 @@ const TestimonialSection = () => {
 
   return (
     <>
-      <PBContainer maxWidth="7xl">
-        <div>TestimonialSection</div>
-        <Carousel
-          plugins={[plugin.current]}
-          className="w-full mx-auto"
-          onMouseEnter={plugin.current.stop}
-          onMouseLeave={plugin.current.reset}
-        >
-          <CarouselContent>
-            {testimonials?.map((itm, index) => (
-              <CarouselItem
-                key={index}
-                className="pl-1 md:basis-1/3 lg:basis-1/4"
-              >
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-4xl font-semibold">{itm.name}</span>
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          {/* <CarouselPrevious />
-          <CarouselNext /> */}
-        </Carousel>
+      <PBContainer className="mt-8 md:mt-14" maxWidth="7xl">
+        <div className=" grid grid-cols-6 items-center gap-6">
+          <div className=" col-span-full md:col-span-2">
+            <div className=" space-y-5">
+              <PBButton>What our users say</PBButton>
+              <h2 className=" text-4xl lg:text-6xl font-bold">
+                Why they love us
+              </h2>
+              <p className=" font-medium">
+                Helping millions of users to buy and sell easily!
+              </p>
+            </div>
+          </div>
+          <div className=" col-span-full md:col-span-4">
+            <Carousel
+              plugins={[plugin.current]}
+              className="w-full mx-auto"
+              onMouseEnter={plugin.current.stop}
+              onMouseLeave={plugin.current.reset}
+            >
+              <CarouselContent>
+                {testimonials?.map((testimonial, index) => (
+                  <CarouselItem key={index} className="pl-1 md:basis-1/2">
+                    <div className="p-1">
+                      <Card className="w-full max-w-sm p-4 hover:shadow-sm border-none bg-white">
+                        <CardContent className="flex flex-col items-start text-start gap-4 mt-5 mb-3">
+                          <span className="text-3xl font-semibold">
+                            {testimonial.name}
+                          </span>
+                          <p className="text-sm text-gray-500">
+                            {testimonial.location}
+                          </p>
+                          <div className="flex gap-1 text-yellow-500">
+                            {Array.from({ length: testimonial.rating }).map(
+                              (_, index) => (
+                                <Star
+                                  key={index}
+                                  size={20}
+                                  fill="currentColor"
+                                  stroke="none"
+                                />
+                              )
+                            )}
+                          </div>
+                          <p className="text-gray-700">
+                            "{testimonial.comment}"
+                          </p>
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
+          </div>
+        </div>
       </PBContainer>
     </>
   );
